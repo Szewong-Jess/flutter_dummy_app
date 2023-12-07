4542ebbeec2169ebbf095b18f8040767b336cf48
Szewong-Jess <szewong1222@gmail.com>
2023-12-07 11:26:57 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:dummy_flutter_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.amber),
+        home: const RootPage());
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({Key? key}) : super(key: key);
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int currenPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        elevation: 2.0,
+        title: const Text('Flutter'),
+      ),
+      body: const HomePage(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('Floating debug button');
+        },
+        child: const Icon(Icons.home),
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'home'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'profile'),
+        ],
+        onDestinationSelected: (int index) {
+          setState(() {
+            currenPage = index;
+          });
+        },
+        selectedIndex: currenPage,
+      ),
+    );
+  }
+}
