3b0bbdcbda1174a5bf9e59d5aed5c90f048a2d4f
Szewong-Jess <szewong1222@gmail.com>
2023-12-07 20:41:00 -0500

    wrap widgets

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -25,8 +25,54 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
       body: Column(
         children: [
           Image.asset('images/einstein.jpg'),
+          const SizedBox(
+            height: 10,
+          ),
           const Divider(
             color: Colors.amber,
+          ),
+          Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              width: double.infinity,
+              color: Colors.blueGrey,
+              child: const Center(
+                child: Text('This is a text widget',
+                    style: TextStyle(color: Colors.white)),
+              )),
+          ElevatedButton(
+              onPressed: () {
+                debugPrint('Elevated Button');
+              },
+              child: const Text('Elevated Button')),
+          OutlinedButton(
+              onPressed: () {
+                debugPrint('Outlines Button');
+              },
+              child: const Text('Outlined Button')),
+          GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () {
+              debugPrint(
+                'This is the row',
+              );
+            },
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(
+                  Icons.local_fire_department,
+                  color: Colors.amber,
+                ),
+                Text(
+                  'Row widgets',
+                ),
+                Icon(
+                  Icons.local_fire_department,
+                  color: Colors.amber,
+                ),
+              ],
+            ),
           )
         ],
       ),
