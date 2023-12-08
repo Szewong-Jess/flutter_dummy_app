955d5b92b24a457f5f590d1aa2cd03ca033d9af3
Szewong-Jess <szewong1222@gmail.com>
2023-12-08 11:18:27 -0500

    single child scroll view

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -24,76 +24,80 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           icon: const Icon(Icons.arrow_back_ios),
         ),
       ),
-      body: Column(
-        children: [
-          Image.asset('images/einstein.jpg'),
-          const SizedBox(
-            height: 10,
-          ),
-          const Divider(
-            color: Colors.amber,
-          ),
-          Container(
-              margin: const EdgeInsets.all(10.0),
-              padding: const EdgeInsets.all(10.0),
-              width: double.infinity,
-              color: Colors.blueGrey,
-              child: const Center(
-                child: Text('This is a text widget',
-                    style: TextStyle(color: Colors.white)),
-              )),
-          ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: isSwitch ? Colors.amberAccent : Colors.blue,
-              ),
-              onPressed: () {
-                debugPrint('Elevated Button');
-              },
-              child: const Text('Elevated Button')),
-          OutlinedButton(
-              onPressed: () {
-                debugPrint('Outlines Button');
-              },
-              child: const Text('Outlined Button')),
-          GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () {
-              debugPrint(
-                'This is the row',
-              );
-            },
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(
-                  Icons.local_fire_department,
-                  color: Colors.amber,
-                ),
-                Text(
-                  'Row widgets',
-                ),
-                Icon(
-                  Icons.local_fire_department,
-                  color: Colors.amber,
-                ),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('images/einstein.jpg'),
+            const SizedBox(
+              height: 10,
             ),
-          ),
-          Switch(
-              value: isSwitch,
-              onChanged: (bool newBool) {
-                setState(() {
-                  isSwitch = newBool;
-                });
-              }),
-          Checkbox(
-              value: isCheckbox,
-              onChanged: (bool? newBool) {
-                setState(() {
-                  isCheckbox = newBool;
-                });
-              })
-        ],
+            const Divider(
+              color: Colors.amber,
+            ),
+            Container(
+                margin: const EdgeInsets.all(10.0),
+                padding: const EdgeInsets.all(10.0),
+                width: double.infinity,
+                color: Colors.blueGrey,
+                child: const Center(
+                  child: Text('This is a text widget',
+                      style: TextStyle(color: Colors.white)),
+                )),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: isSwitch ? Colors.amberAccent : Colors.blue,
+                ),
+                onPressed: () {
+                  debugPrint('Elevated Button');
+                },
+                child: const Text('Elevated Button')),
+            OutlinedButton(
+                onPressed: () {
+                  debugPrint('Outlines Button');
+                },
+                child: const Text('Outlined Button')),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                debugPrint(
+                  'This is the row',
+                );
+              },
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.amber,
+                  ),
+                  Text(
+                    'Row widgets',
+                  ),
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.amber,
+                  ),
+                ],
+              ),
+            ),
+            Switch(
+                value: isSwitch,
+                onChanged: (bool newBool) {
+                  setState(() {
+                    isSwitch = newBool;
+                  });
+                }),
+            Checkbox(
+                value: isCheckbox,
+                onChanged: (bool? newBool) {
+                  setState(() {
+                    isCheckbox = newBool;
+                  });
+                }),
+            Image.network(
+                'https://hips.hearstapps.com/hmg-prod/images/worlds-smallest-dogs-1647336453.jpg?crop=0.493xw:0.987xh;0.505xw,0.00326xh&resize=1200:*')
+          ],
+        ),
       ),
     );
   }
