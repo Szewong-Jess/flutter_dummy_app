7c38fc828d00521f631db31923f44863bcff9620
Szewong-Jess <szewong1222@gmail.com>
2023-12-08 12:48:59 -0500

    item count

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -8,6 +8,7 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      itemCount: itemCount,
       itemBuilder: (BuildContext context, int index) {
         return ListTile(
           title: Text('Item ${(index + 1)}'),
