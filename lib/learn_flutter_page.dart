4aa3bce1b084a722ff55a14f50c89e7321cdd91e
Szewong-Jess <szewong1222@gmail.com>
2023-12-08 11:21:41 -0500

    app bar added action

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -23,6 +23,16 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           },
           icon: const Icon(Icons.arrow_back_ios),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              debugPrint('appbar action');
+            },
+            icon: const Icon(
+              Icons.info_outline,
+            ),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
